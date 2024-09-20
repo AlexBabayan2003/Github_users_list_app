c77d9989d0c582369d50f72dc555605cc43124a3
AlexBabayan2003 <bvriedteenz@gmail.com>
2024-09-20 13:03:12 +0400

    UI update

--- app/src/main/java/com/example/ameriatask/userdetailsscreen/UserDetailScreen.kt
+++ app/src/main/java/com/example/ameriatask/userdetailsscreen/UserDetailScreen.kt
@@ -6,14 +6,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,19 +30,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
+import com.example.ameriatask.R
 import com.example.ameriatask.data.GithubUserDetails
 
 @Composable
 fun UserDetailScreen(
     username: String,
     viewModel: UserDetailsViewModel = hiltViewModel(),
-) {
-
-
-    val user: GithubUserDetails? by viewModel.user.collectAsState()
+) {val user: GithubUserDetails? by viewModel.user.collectAsState()
     LaunchedEffect(key1 = username) {
         viewModel.getUserDetails(username)
     }
@@ -44,61 +49,100 @@ fun UserDetailScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(16.dp),horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Avatar
         if (user != null) {
-            user?.run {
-                Image(
-                    painter = rememberAsyncImagePainter(avatarUrl),
-                    contentDescription = null,
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Column(
                     modifier = Modifier
-                        .size(200.dp)
-                        .clip(CircleShape)
-                )
-                Text(text = name ?: "No name", style = MaterialTheme.typography.headlineLarge)
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    user?.run {
 
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        imageVector = Icons.Filled.LocationOn,
-                        contentDescription = "Location Icon",
-                        modifier = Modifier.size(16.dp)
+                    Image(
+                        painter = rememberAsyncImagePainter(avatarUrl),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(200.dp)
+                            .clip(CircleShape)
                     )
                     Spacer(modifier = Modifier.height(16.dp))
-
                     Text(
-                        text = location ?: "Unknown location",
-                        style = MaterialTheme.typography.bodyLarge
+                        text = name ?: "No name",
+                        style = MaterialTheme.typography.headlineMedium
                     )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            imageVector = Icons.Filled.LocationOn,
+                            contentDescription = "Location Icon",
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = location ?: "Unknown location",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Text(
+                                text = "Followers: $followers",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Icon(
+                                painter = painterResource(id = R.drawable.vertical_line_svgrepo_com),
+                                contentDescription = null,
+                                modifier = Modifier.size(16.dp)
+                            )
+                            Text(
+                                text = "Following: $following",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        HorizontalDivider()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        bio?.let {
+                            Text(text = "Bio: $it", style = MaterialTheme.typography.bodyMedium)
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        publicRepos?.let {
+                            Text(
+                                text = "Public Repos: $it",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        publicGists?.let {
+                            Text(
+                                text = "Public Gists: $it",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        updatedAt?.let {
+                            Text(
+                                text = "Updated at: $it",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
                 }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // Followers and Following
-                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
-                    Text(text = "Followers: $followers")
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Following: $following")
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-                Column(horizontalAlignment = Alignment.Start) {
-                    bio?.let {
-                        Text(text = "Bio: \n$it", style = MaterialTheme.typography.bodyLarge)
-                    }
-                    publicRepos?.let {
-                        Text(text = "Public Repos: \n$it")
-                    }
-                    publicGists?.let {
-                        Text(text = "Public Gists: \n$it")
-                    }
-                    updatedAt?.let {
-                        Text(text = "Updated at: \n$it")
-                    }
-                }
-
             }
-        } else CircularProgressIndicator()
+        } else {
+            CircularProgressIndicator()
+        }
     }
 }
